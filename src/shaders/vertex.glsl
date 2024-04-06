9f4efeeb846bb6be801e01377c1c5e21022aabb1
Mattia <82611659+Xhst@users.noreply.github.com>
2024-04-06 18:41:01 +0200

    docs: vertex shader comments

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,5 +1,7 @@
+// Define the attribute for the position of the vertex
 attribute vec2 position;
 
 void main() {
+    // Set the position of the vertex in clip space
     gl_Position = vec4(position, 0, 1);
 }
