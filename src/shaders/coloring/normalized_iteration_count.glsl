a0eb5adce50c551f14fa3b5a1548263222e26c01
Mattia <82611659+Xhst@users.noreply.github.com>
2024-04-08 13:04:03 +0200

    feat: normalized iteration count coloring

---
+++ src/shaders/coloring/normalized_iteration_count.glsl
@@ -0,0 +1,13 @@
+
+iterations = iterations - (iterations / 16) * 16;
+
+// Calculate the normalized iteration count
+float normalizedIterations = float(iterations) + 1.0 - (log(log(length(z)) / log(2.0))) / log(2.0);
+
+int idx = int(normalizedIterations);
+for (int i = 0; i < 16; i++) {
+    if (i == idx) {
+        color = mix(colors[i], colors[i + 1], fract(normalizedIterations));
+        break;
+    }
+}
