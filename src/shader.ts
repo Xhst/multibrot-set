185c0433a7b5bdbe7ffb3574e09e8e035e78d937
Mattia <82611659+Xhst@users.noreply.github.com>
2024-04-08 15:03:50 +0200

    docs: shader

--- src/shader.ts
+++ src/shader.ts
@@ -2,6 +2,9 @@ import fragmentShaderSource from './shaders/fragment.glsl';
 import vertexShaderSource from './shaders/vertex.glsl';
 import { Color } from './colors';
 
+/**
+ * Represents a shader used for rendering fractals.
+ */
 export class Shader {
 
     private readonly shaderOptions: ShaderOptions;
@@ -22,34 +25,65 @@ export class Shader {
         }
     }
 
+    /**
+     * Updates the maximum number of iterations for the fractal calculation.
+     * @param maxIterations - The maximum number of iterations.
+     */
     public updateMaxIterations(maxIterations: number): void {
         this.shaderOptions.maxIterations = maxIterations;
     }
 
+    /**
+     * Updates the bail-out value for the fractal calculation.
+     * @param bailOut - The bail-out value.
+     */
     public updateBailOut(bailOut: number): void {
         this.shaderOptions.bailOut = bailOut;
     }
 
+    /**
+     * Updates the colors used in the shader.
+     * @param colors - An array of colors.
+     */
     public updateColors(colors: Array<Color>): void {
         this.shaderOptions.colors = colors;
     }
 
+    /**
+     * Updates the uniforms used in the shader program.
+     * @param uniforms - Uniforms string for the shader program.
+     */
     public updateUniforms(uniforms: string): void {
         this.shaderOptions.uniforms = uniforms;
     }
 
+    /**
+     * Updates the variables used in the shader program.
+     * @param variables - Variables string for the shader program.
+     */
     public updateVariables(variables: string): void {
         this.shaderOptions.variables = variables;
     }
 
+    /**
+     * Updates the iterations algorithm used in the shader program.
+     * @param iterationsAlgorithm - Iterations algorithm string for the shader program.
+     */
     public updateIterationsAlgorithm(iterationsAlgorithm: string): void {
         this.shaderOptions.iterationsAlgorithm = iterationsAlgorithm;
     }
 
+    /**
+     * Updates the coloring algorithm used in the shader program.
+     * @param coloringAlgorithm - Coloring algorithm string for the shader program.
+     */
     public updateColoringAlgorithm(coloringAlgorithm: string): void {
         this.shaderOptions.coloringAlgorithm = coloringAlgorithm;
     }
 
+    /**
+     * Assembles the source code of the shader program.
+     */
     public assembleProgramSource(): void {
         this.wrappedFragmentShaderSource = fragmentShaderSource
             .replace('@MAX_ITERATIONS@', this.shaderOptions.maxIterations.toString())
@@ -63,6 +97,10 @@ export class Shader {
         this.wrappedVertexShaderSource = vertexShaderSource;
     }
 
+    /**
+     * Gets the colors used in the shader.
+     * @returns An array of colors.
+     */
     public getColors(): Array<Color> {
         return this.shaderOptions.colors;
     }
@@ -76,6 +114,9 @@ export class Shader {
     }
 }
 
+/**
+ * Represents options for configuring a shader.
+ */
 export type ShaderOptions = {
     maxIterations: number;
     bailOut: number;
