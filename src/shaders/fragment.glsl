fc6ef52aa533c66722f968a9700725336972abce
Mattia <82611659+Xhst@users.noreply.github.com>
2024-04-08 13:03:20 +0200

    feat: parametrized fragment

---
+++ src/shaders/fragment.glsl
@@ -0,0 +1,26 @@
+
+precision mediump float;
+
+const int MAX_ITERATIONS = @MAX_ITERATIONS@;
+
+uniform vec2 resolution;
+uniform vec2 offset;
+
+uniform float scale;
+
+uniform vec3 colors[@COLORS_COUNT@];
+
+@UNIFORMS@
+
+void main() {
+    vec3 color = vec3(0.0, 0.0, 0.0);
+    vec2 scale2 = vec2(scale, scale * resolution.y / resolution.x);
+
+    @VARIABLES@
+    
+    @ITERATIONS_ALGORITHM@
+
+    @COLORING_ALGORITHM@
+
+    gl_FragColor = vec4(color, 1.0);
+}
